32750a5814fe73e1d63738f5d28c1264e17666fb
KHAKhazeus <khazeus@outlook.com>
2018-12-28 13:22:48 +0800

    Offline Protection

--- app/src/main/java/com/kha/cbc/comfy/presenter/AvatarPresenter.kt
+++ app/src/main/java/com/kha/cbc/comfy/presenter/AvatarPresenter.kt
@@ -70,18 +70,20 @@ open class AvatarPresenter(override val view: AvatarView): LeanCloudPresenter(vi
             query.whereEqualTo("username", User.username)
             query.findInBackground(object: FindCallback<AVObject>(){
                 override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
-                    val userObject = p0!![0]
-                    val avatarQuery = AVQuery<AVObject>("ComfyUserInfoMap")
-                    avatarQuery.whereEqualTo("user", userObject)
-                    avatarQuery.include("avatar")
-                    avatarQuery.findInBackground(object: FindCallback<AVObject>(){
-                        override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
-                            if(p0 != null && p0.size > 0){
-                                val avatar = p0[0].getAVFile<AVFile>("avatar")
-                                view.downloadAvatarFinish(avatar.url)
+                    if(p1 == null && p0 != null && p0.size > 0){
+                        val userObject = p0!![0]
+                        val avatarQuery = AVQuery<AVObject>("ComfyUserInfoMap")
+                        avatarQuery.whereEqualTo("user", userObject)
+                        avatarQuery.include("avatar")
+                        avatarQuery.findInBackground(object: FindCallback<AVObject>(){
+                            override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
+                                if(p0 != null && p0.size > 0){
+                                    val avatar = p0[0].getAVFile<AVFile>("avatar")
+                                    view.downloadAvatarFinish(avatar.url)
+                                }
                             }
-                        }
-                    })
+                        })
+                    }
                 }
             })
         }
@@ -146,19 +148,20 @@ open class AvatarPresenter(override val view: AvatarView): LeanCloudPresenter(vi
         query.whereEqualTo("username", User.username)
         query.findInBackground(object: FindCallback<AVObject>(){
             override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
-                val userObject = p0!![0]
-                val avatarQuery = AVQuery<AVObject>("ComfyUserInfoMap")
-                avatarQuery.whereEqualTo("user", userObject).include("avatar")
-                avatarQuery.findInBackground(object: FindCallback<AVObject>(){
-                    override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
-                        if(p0 != null && p0.isNotEmpty()){
-                            val prevMap = p0[0]
-                            val prevAvatar = prevMap.getAVFile<AVFile>("avatar")
-                            prevAvatar.deleteInBackground()
-                            prevMap.deleteInBackground()
-                        }
-                        val sourceFile = uri.path
-                        val uploadFile: AVFile
+                if(p1 == null && p0 != null && p0.size > 0){
+                    val userObject = p0!![0]
+                    val avatarQuery = AVQuery<AVObject>("ComfyUserInfoMap")
+                    avatarQuery.whereEqualTo("user", userObject).include("avatar")
+                    avatarQuery.findInBackground(object: FindCallback<AVObject>(){
+                        override fun done(p0: MutableList<AVObject>?, p1: AVException?) {
+                            if(p0 != null && p0.isNotEmpty()){
+                                val prevMap = p0[0]
+                                val prevAvatar = prevMap.getAVFile<AVFile>("avatar")
+                                prevAvatar.deleteInBackground()
+                                prevMap.deleteInBackground()
+                            }
+                            val sourceFile = uri.path
+                            val uploadFile: AVFile
 //                        if(sourceFile == "/mipmap/ic_launcher_round"){
 //                            val file = File(
 //                                URI.create(Uri.Builder().scheme("res").path("R.mipmap.ic_launcher_round")
@@ -170,37 +173,38 @@ open class AvatarPresenter(override val view: AvatarView): LeanCloudPresenter(vi
                             uploadFile = AVFile.withAbsoluteLocalPath(User.username + "avatar.jpg"
                                 , sourceFile)
 //                        }
-                        uploadFile.saveInBackground(object: SaveCallback(){
-                            override fun done(p0: AVException?) {
-                                val mapObject = AVObject("ComfyUserInfoMap")
-                                mapObject.put("user", userObject)
-                                mapObject.put("avatar", uploadFile)
-                                mapObject.saveInBackground(object :SaveCallback(){
-                                    override fun done(p0: AVException?) {
-                                        userObject.put("avatarUrl",uploadFile.url)
-                                        userObject.saveInBackground(object: SaveCallback(){
-                                            override fun done(p0: AVException?) {
-                                                view.uploadAvatarFinish(uploadFile.url)
-                                                try {
-                                                    view.avatarDao.deleteAll()
-                                                    val historyAvatar = GDAvatar(User.username, uploadFile.url)
-                                                    view.avatarDao.insert(historyAvatar)
+                            uploadFile.saveInBackground(object: SaveCallback(){
+                                override fun done(p0: AVException?) {
+                                    val mapObject = AVObject("ComfyUserInfoMap")
+                                    mapObject.put("user", userObject)
+                                    mapObject.put("avatar", uploadFile)
+                                    mapObject.saveInBackground(object :SaveCallback(){
+                                        override fun done(p0: AVException?) {
+                                            userObject.put("avatarUrl",uploadFile.url)
+                                            userObject.saveInBackground(object: SaveCallback(){
+                                                override fun done(p0: AVException?) {
+                                                    view.uploadAvatarFinish(uploadFile.url)
+                                                    try {
+                                                        view.avatarDao.deleteAll()
+                                                        val historyAvatar = GDAvatar(User.username, uploadFile.url)
+                                                        view.avatarDao.insert(historyAvatar)
+                                                    }
+                                                    catch (e: Exception){
+                                                        e.printStackTrace()
+                                                    }
                                                 }
-                                                catch (e: Exception){
-                                                    e.printStackTrace()
-                                                }
-                                            }
-                                        })
-                                    }
-                                })
-                            }
-                        },object: ProgressCallback(){
-                            override fun done(p0: Int?) {
-                                view.uploadProgressUpdate(p0)
-                            }
-                        })
-                    }
-                })
+                                            })
+                                        }
+                                    })
+                                }
+                            },object: ProgressCallback(){
+                                override fun done(p0: Int?) {
+                                    view.uploadProgressUpdate(p0)
+                                }
+                            })
+                        }
+                    })
+                }
             }
         })
 
